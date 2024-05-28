686169b9ba47fe1f6f58401880213b5dbd36ff5a
TedrosTK <tedydada@gmail.com>
2024-05-28 19:37:19 -0400

    Year and enter key event listener

--- app.js
+++ app.js
@@ -15,17 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentPage = 1;
     let totalPages = 1;
 
-    searchBtn.addEventListener('click', () => {
-        const keyword = searchInput.value.trim();
-        if (keyword) {
-            fetchAndDisplayMovies(keyword, 1);
-        }
-    });
+    // Add event listener for search button click
+    searchBtn.addEventListener('click', searchHandler);
+
+    // Add event listener for Enter key press in the search input field
+    searchInput.addEventListener('keyup', searchHandler);
+
+    // searchBtn.addEventListener('click', () => {
+    //     const keyword = searchInput.value.trim();
+    //     if (keyword) {
+    //         fetchAndDisplayMovies(keyword, 1);
+    //     }
+    // });
 
     clearBtn.addEventListener('click', () => {
         searchInput.value = '';
-        filmsList.innerHTML = '';
+        // filmsList.innerHTML = '';
         pagination.innerHTML = '';
+        clearSearchResultsText();
+        fetchAndDisplayRandomMovies();
     });
 
     pagination.addEventListener('click', (event) => {
@@ -61,13 +69,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 throw new Error('Network response was not ok ' + response.statusText);
             }
             const data = await response.json();
+            console.log(data)
             return data;
         } catch (error) {
             console.error('Fetch error: ', error);
             alert('Failed to fetch movies. Please try again later.');
         }
     }
-
     async function fetchPopularMovies() {
         try {
             const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
@@ -85,12 +93,25 @@ document.addEventListener('DOMContentLoaded', () => {
     async function fetchAndDisplayMovies(keyword, page) {
         loadingSpinner.classList.add('show');
         try {
-            
-            const movies = await fetchMovies(keyword, page);
-            totalPages = movies.total_pages;
+            const moviesData = await fetchMovies(keyword, page);
+            const movies = moviesData.results;
+            totalPages = Math.ceil(movies.length / 6);
             currentPage = page;
-            displayMovies(movies.results);
+            const start = (currentPage - 1) * 6;
+            const end = start + 6;
+            const paginatedMovies = movies.slice(start, end);
+            displayMovies(paginatedMovies);
             displayPagination(totalPages, currentPage);
+            updateFilmsListClass(paginatedMovies.length);
+            if (keyword) {
+                displaySearchResultsText();
+            }
+            
+            // const movies = await fetchMovies(keyword, page);
+            // totalPages = movies.total_pages;
+            // currentPage = page;
+            // displayMovies(movies.results);
+            // displayPagination(totalPages, currentPage);
         } finally {
             loadingSpinner.classList.remove('show');
         }
@@ -107,28 +128,91 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+
+    function initiateSearch() {
+        const keyword = searchInput.value.trim();
+        if (keyword) {
+            fetchAndDisplayMovies(keyword, 1);
+        }
+    }
+
+    // Event listener for both click and Enter key press
+    function searchHandler(event) {
+        if (event.type === 'click' || (event.type === 'keyup' && event.keyCode === 13)) {
+            event.preventDefault(); // Prevent the default action (form submission) for Enter key press
+            initiateSearch(); // Call the search function
+        }
+    }
+
+
     function getRandomMovies(movies, count) {
         const shuffled = movies.sort(() => 0.5 - Math.random());
         return shuffled.slice(0, count);
     }
 
     function displayMovies(movies) {
+        const placeholderImage = 'https://placeholder.pics/svg/500x750/DAD7DA/0C0C0C-F9FFF6/No%20Image%20available'; // Placeholder image UR
         filmsList.innerHTML = movies.map(movie => `
             <div class="movie">
+                
                 <div class="movie__cover">
-                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" loading="lazy">
+                    <img src="${movie.poster_path ? 'https://image.tmdb.org/t/p/w500' + movie.poster_path : placeholderImage}" alt="${movie.title}" loading="lazy">
                 </div>
-                <div class="movie__title">${movie.title}</div>
+                <div class="movie__title">${movie.title} (${extractYear(movie.release_date)})</div>
+                
             </div>
         `).join('');
+        
     }
 
     function displayPagination(totalPages, currentPage) {
         pagination.innerHTML = `
-            <button class="pagination-btn" data-page="prev" ${currentPage === 1 ? 'disabled' : ''}>Previous Page</button>
-            <button class="pagination-btn" data-page="next" ${currentPage === totalPages ? 'disabled' : ''}>Next Page</button>
-        `;
+        <button class="pagination-btn" data-page="prev" ${currentPage === 1 ? 'disabled' : ''}>Previous Page</button>
+        <span>Page ${currentPage} of ${totalPages}</span>
+        <button class="pagination-btn" data-page="next" ${currentPage === totalPages ? 'disabled' : ''}>Next Page</button>
+    `;
+        // pagination.innerHTML = `
+        //     <button class="pagination-btn" data-page="prev" ${currentPage === 1 ? 'disabled' : ''}>Previous Page</button>
+        //     <button class="pagination-btn" data-page="next" ${currentPage === totalPages ? 'disabled' : ''}>Next Page</button>
+        // `;
     }
+    function updateFilmsListClass(numberOfMovies) {
+        if (numberOfMovies <= 6) {
+            filmsList.classList.remove('filmsList--many');
+            filmsList.classList.add('filmsList--few');
+        } else {
+            filmsList.classList.remove('filmsList--few');
+            filmsList.classList.add('filmsList--many');
+        }
+    }
+
+    function displaySearchResultsText() {
+        const moviesContainer = document.getElementById('searchResults');
+        const searchResultsText = document.createElement('div');
+        searchResultsText.textContent = 'Search Results...';
+        searchResultsText.classList.add('search-results-text'); // Add a class for styling
+        
+        // Check if search results text already exists, if not, append it
+        if (!document.querySelector('.search-results-text')) {
+            moviesContainer.insertAdjacentElement('afterbegin', searchResultsText);
+        }
+    }
+    
+    function clearSearchResultsText() {
+        const searchResultsText = document.querySelector('.search-results-text');
+        if (searchResultsText) {
+            searchResultsText.remove();
+        }
+    }
+
+    function extractYear(releaseDate) {
+        return releaseDate.split('-')[0];
+    }
+
+
+    
+
+    
 
     // Fetch and display random movies when the page first loads
     fetchAndDisplayRandomMovies();
